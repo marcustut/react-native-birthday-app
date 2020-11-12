e5f16b749c342b315f77a6097389191ab54a72dc
Marcus Lee <marcustutorial@hotmail.com>
2020-11-12 16:12:31 +0800

    changed 'Home Screen' to 'Not Home Screen'

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -8,7 +8,7 @@ import { Text, View } from '../components/Themed';
 export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home Screen</Text>
+      <Text style={styles.title}>Not Home Screen</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.js" />
       <Button
